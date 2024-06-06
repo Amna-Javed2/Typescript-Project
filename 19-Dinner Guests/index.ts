c36381a3638dc6c68e69d4b00657c723eae1e362
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-06 12:52:20 -0700

    Add files via upload

---
+++ 19-Dinner Guests/index.ts
@@ -0,0 +1,6 @@
+// Dinner Guests: Working with one of the programs from Exercises
+// 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guests : string[] = ['Fatima','Amna','Farzana','Noor','Mirha','Jannat']; 
+console.log(`I am inviting ${guests.length} people to dinner.`);
+
