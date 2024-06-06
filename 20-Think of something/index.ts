f6f6845e8eb0095b70e230af6461b6507c0e7f38
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-06 13:05:36 -0700

    Add files via upload

---
+++ 20-Think of something/index.ts
@@ -0,0 +1,51 @@
+/*Question 23: Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+
+Explain & TIP: Conditional tests check if something is true or false. Writing different tests helps you
+understand how conditions work in programming.*/
+
+
+let num: number = 10;
+
+
+// Test 1
+console.log("Is num equal to 10? I predict True.");
+console.log(num == 10);
+
+// Test 2
+console.log("Is num not equal to 5? I predict True.");
+console.log(num != 5);
+
+// Test 3
+console.log("Is num greater than 5? I predict True.");
+console.log(num > 5);
+
+// Test 4
+console.log("Is num less than or equal to 10? I predict True.");
+console.log(num <= 10);
+
+// Test 5
+console.log("Is num not equal to '10'? I predict false.");
+console.log(num !== 10);
+
+let name1 = 'John';
+
+// Test 6
+console.log("Is name equal to 'John'? I predict True.");
+console.log(name1 == 'John');
+
+// Test 7
+console.log("Is name not equal to 'Mary'? I predict True.");
+console.log(name1 != 'Mary');
+
+// Test 8
+console.log("Is name length greater than 3? I predict True.");
+console.log(name1.length > 3);
+
+// Test 9
+console.log("Is name equal to 10? I predict True.");
+console.log(num === 10);
+
+// Test 10
+console.log("Is num less than or equal to 10? I predict True.");
+console.log(num >= 10);
+
