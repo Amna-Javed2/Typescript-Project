7d3477adc8e6357106f0eb406e3da935b2ed5232
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-06 12:53:50 -0700

    Update index.js

--- 19-Dinner Guests/index.js
+++ 19-Dinner Guests/index.js
@@ -1,9 +1,7 @@
 "use strict";
-/*Question 19: Dinner Guests: Use one of your programs from Exercises 14 through
-18 to print a message indicating the number of people you are inviting to dinner.
+// Dinner Guests: Working with one of the programs from Exercises
+// 14 through 18, print a message indicating the number of people you are inviting to dinner.
 
-Explain & TIP: Arrays have a length property that tells you how many items are in
- the list. Use this to inform your guests how big the party will be */
 Object.defineProperty(exports, "__esModule", { value: true });
 let guests = ['Fatima', 'Amna', 'Farzana', 'Noor', 'Mirha', 'Jannat'];
 console.log(`I am inviting ${guests.length} people to dinner.`);
