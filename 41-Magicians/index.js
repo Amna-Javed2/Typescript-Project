32f9e346b2f7f490fb00fdfbcb0eb1381028f119
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 03:07:37 -0700

    Add files via upload

---
+++ 41-Magicians/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let magicians = ["A", "B", "C"];
+function show_magicians(magicians) {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ 41-Magicians/index.ts
@@ -0,0 +1,12 @@
+
+
+let magicians: string[] = ["A", "B", "C"];
+
+function show_magicians(magicians: string[]) {
+  magicians.forEach((magician) => {
+    console.log(magician);
+  });
+}
+
+show_magicians(magicians);
+
