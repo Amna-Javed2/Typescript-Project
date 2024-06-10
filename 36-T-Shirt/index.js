4df3e93faee8fbdde927c339d155db88298cba9b
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 03:01:28 -0700

    Add files via upload

---
+++ 36-T-Shirt/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size, message) {
+    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+}
+make_shirt("medium", "Hello kitty");
