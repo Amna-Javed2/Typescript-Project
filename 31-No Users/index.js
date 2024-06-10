4e17ad86d2b335fe159a82ef9fe2806bdd6e42c8
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:46:25 -0700

    Add files via upload

---
+++ 31-No Users/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+Question 31: No Users: Ensure your user list isn’t empty.
+
+Explain & TIP: Before greeting users, check if there are any. This teaches you to handle empty arrays.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Greet Users 
+}
---
+++ 31-No Users/index.ts
@@ -0,0 +1,15 @@
+/*
+Question 31: No Users: Ensure your user list isn’t empty.
+
+Explain & TIP: Before greeting users, check if there are any. This teaches you to handle empty arrays.*/
+
+
+let usernames: string[] = [];
+
+if(usernames.length === 0){
+    console.log("We need to find some users!");
+} 
+else {
+    // Greet Users 
+}
+
