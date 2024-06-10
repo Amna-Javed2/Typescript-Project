d3e6eda99ddee5fee0f7704227055467160ea770
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:31:28 -0700

    Update index.ts

--- 22-Intentional Error/index.ts
+++ 22-Intentional Error/index.ts
@@ -1,4 +1,4 @@
-/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.
+/* Question 22 Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.
 Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
 
 Explain & TIP: Array index errors happen when you try to access a position in the array that doesn’t exist. Experimenting with 
