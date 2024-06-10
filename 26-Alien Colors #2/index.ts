6e15d19c691b9a16821fa2c06503d70de29c5e75
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:41:22 -0700

    Add files via upload

---
+++ 26-Alien Colors #2/index.ts
@@ -0,0 +1,12 @@
+/*Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+
+Explain & TIP: Expanding on the previous exercise, if-else chains allow you to handle multiple conditions.*/
+
+let alienColor = "blue";
+
+if(alienColor == "green"){
+    console.log("The player  earned 5 points for shooting the green alien.");
+}
+else if(alienColor != "green"){
+    console.log("The player earned 10 points for shooting another alien");
+}
