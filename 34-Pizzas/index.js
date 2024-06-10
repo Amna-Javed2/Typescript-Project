20511d4866f354d51cbc0d1e45cdfa8df6571f90
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:58:29 -0700

    Add files via upload

---
+++ 34-Pizzas/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let pizzas = ["pizza1", "pizza2", "pizza3"];
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza}.`);
+});
+console.log("I really love pizza!");
---
+++ 34-Pizzas/index.ts
@@ -0,0 +1,9 @@
+
+let pizzas: string[] = ["pizza1", "pizza2", "pizza3"];
+
+pizzas.forEach(pizza => {
+console.log(`I like ${pizza}.`);
+});
+
+console.log("I really love pizza!");
+
