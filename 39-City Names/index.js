2ab71eef9581e4d2d841e4bd51e8b2e606f1dfe8
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 03:05:18 -0700

    Add files via upload

---
+++ 39-City Names/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
