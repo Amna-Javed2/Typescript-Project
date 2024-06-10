d54065886c8bc62ae889c87903f20cb997c11256
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:44:10 -0700

    Add files via upload

---
+++ 29-Favorite Fruit/index.js
@@ -0,0 +1,24 @@
+"use strict";
+/*Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+
+Explain & TIP: Arrays can store multiple items like fruit names. Use if statements to check for specific fruits.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let fvrt_fruits = ["Apple", "Mango", "Orange", "Grapes"];
+if (fvrt_fruits.includes("Apple")) {
+    console.log("You really like Apples!");
+}
+if (fvrt_fruits.includes("Mango")) {
+    console.log("You really like mangoes!");
+}
+if (fvrt_fruits.includes("Orange")) {
+    console.log("You really like oranges!");
+}
+if (fvrt_fruits.includes("Grapes")) {
+    console.log("You really like grapes!");
+}
+if (fvrt_fruits.includes("Banana")) {
+    console.log("You really like banana!");
+}
+else {
+    console.log("You don't like banana!");
+}
