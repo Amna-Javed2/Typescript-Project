62d06e4089ec0cf0ba1eab96c1e148d570468fd8
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:37:13 -0700

    Add files via upload

---
+++ 25-Alien Colors #1/index.ts
@@ -0,0 +1,18 @@
+/*Question 25: Alien Colors #1: Imagine an alien was just shot down in a game.
+Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+Explain & TIP: Use an if statement to check the alien's color. This introduces you to simple conditionals.*/
+// Assign a color to the alien
+
+let alienColor: string = 'green';
+
+if (alienColor === 'green') {
+    console.log("The alien is green!");
+} else if (alienColor === 'yellow') {
+    console.log("The alien is yellow!");
+} else if(alienColor === 'red'){
+    console.log("The alien is red!");
+}else{
+    console.log("The alien is an invalid colour!")
+}
+
