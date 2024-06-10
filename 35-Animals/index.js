bc5a5bd98a9e6249b4db9f17be3b0dd61894b77d
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 03:00:44 -0700

    Add files via upload

---
+++ 35-Animals/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let animals = ['dog', 'cat', 'goat', 'rabit'];
+animals.forEach(animal => {
+    console.log(`${animal} are my favourite.`);
+});
+console.log("Any of these animals would make a great pet!");
---
+++ 35-Animals/index.ts
@@ -0,0 +1,9 @@
+
+
+let animals: string[] = ['dog', 'cat', 'goat', 'rabit'];
+
+animals.forEach(animal => {
+    console.log(`${animal} are my favourite.`);
+});
+console.log("Any of these animals would make a great pet!");
+
