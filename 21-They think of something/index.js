d54da1bfbdc3138334de11c3af83663b6a161cd2
Amna javed <160821158+Amna-Javed2@users.noreply.github.com>
2024-06-10 02:26:35 -0700

    Add files via upload

---
+++ 21-They think of something/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+Explain & TIP: Objects in TypeScript can store various related data about a single entity. Think of an object as a container for characteristics
+of something, like a book or a car.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let book = {
+    author: "Amna",
+    title: "Poetry",
+    yearPublished: 2021
+};
+console.log(`Author : ${book.author},
+Title : ${book.title},  
+yearPublished : ${book.yearPublished}`);
---
+++ 21-They think of something/index.ts
@@ -0,0 +1,16 @@
+/*Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+Explain & TIP: Objects in TypeScript can store various related data about a single entity. Think of an object as a container for characteristics
+of something, like a book or a car.*/
+
+
+let book = {
+    author: "Amna",
+    title: "Poetry",
+    yearPublished: 2021
+};
+
+
+console.log(`Author : ${book.author},
+Title : ${book.title},  
+yearPublished : ${book.yearPublished}`);
